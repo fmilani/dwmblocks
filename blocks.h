0b771f2119827c451869b7157a475adfbfee73e7
Felipe Milani <felmilani@gmail.com>
2023-10-22 22:14:05 -0300

    add packages block

--- blocks.h
+++ blocks.h
@@ -1,6 +1,7 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
+	{"",	"sb-packages",	1800,	8},
 	{"",	"sb-memory",	10,	14},
 
 	{"",	"sb-internet",	5,	4},
