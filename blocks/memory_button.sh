63a1db09e5fc748d9651d8de979d8747537f3711
Felipe Milani <felmilani@gmail.com>
2024-08-18 07:58:31 -0300

    improvements to memory and package blocks
    memory: show the combined subprocesses memory
    and uses dmenu to kill the selected process
    
    package: reflect on the change on tmux-launch
    no longer having a session parameter, running
    the update-packages in the attached session

--- blocks/memory_button.sh
+++ blocks/memory_button.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
 case $1 in
-	1) notify-send "🧠 Memory hogs" "$(ps axch -o cmd:15,%mem --sort=-%mem | head)" ;;
+	1) ps -eo comm,rss --no-headers \
+		| awk '{a[$1]+=$2} END {for (i in a) printf "%.2f GB - %s\n", a[i]/1024/1024, i}' \
+		| sort -rn | head -n 10 | dmenu -p "pkill -15" -l 10 | awk -F ' - ' '{print $2}' \
+		| xargs pkill ;;
 	3) setsid -f "$TERMINAL" -e htop ;;
 esac
 
--- blocks/packages_button.sh
+++ blocks/packages_button.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 case $1 in
-	1) tmux-launch home update update-packages ;;
+	1) tmux-launch update update-packages ;;
 	3) notify-send "$(/usr/bin/pacman -Qu)" ;;
 esac
