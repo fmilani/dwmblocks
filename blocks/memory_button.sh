340c4e3529f592de10417e1e021bd6057de04a23
Felipe Milani <felmilani@gmail.com>
2024-10-15 13:46:54 -0300

    use tmux to open htop on memory_button

--- blocks/memory_button.sh
+++ blocks/memory_button.sh
@@ -5,6 +5,6 @@ case $1 in
 		| awk '{a[$1]+=$2} END {for (i in a) printf "%.2f GB - %s\n", a[i]/1024/1024, i}' \
 		| sort -rn | head -n 10 | dmenu -p "pkill -15" -l 10 | awk -F ' - ' '{print $2}' \
 		| xargs pkill ;;
-	3) setsid -f "$TERMINAL" -e htop ;;
+	3) tmux-launch mem htop ;;
 esac
 
