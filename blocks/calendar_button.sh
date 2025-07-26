2f88439de8a4ac745d41df21f2c5a9b843ad32c1
Felipe Milani <felmilani@gmail.com>
2025-07-26 09:16:20 -0300

    fix calendar formatting

--- blocks/calendar_button.sh
+++ blocks/calendar_button.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 case $1 in
-  1) notify-send "This Month" "$(cal | sed "s/\<$(date +'%e')\>/<b><span color='red'>&<\/span><\/b>/")" ;;
+  1) notify-send "This Month" "$(cal | sed "s/\<$(date +'%e' | cut -c2-)\>/<b><span color='red'>&<\/span><\/b>/")" ;;
 esac
