67f321797bab42ee5f159fb9bac04ffa164da273
Felipe Milani <felmilani@gmail.com>
2025-07-26 09:20:36 -0300

    improve memory listing: group subprocesses for more accuracy

--- blocks/memory_button.sh
+++ blocks/memory_button.sh
@@ -1,9 +1,34 @@
 #!/bin/sh
 
 case $1 in
-	1) ps -eo comm,rss --no-headers \
-		| awk '{a[$1]+=$2} END {for (i in a) printf "%.2f GB - %s\n", a[i]/1024/1024, i}' \
-		| sort -rn | head -n 10 | dmenu -p "pkill -15" -l 10 | awk -F ' - ' '{print $2}' \
+	1) ps -eo rss,args --no-headers \
+  | awk -v maxlen=20 '{
+    rss = $1
+    $1 = ""
+    sub(/^ /, "", $0)
+    split($0, a, " ")
+    cmd = a[1]
+    n = split(cmd, parts, "/")
+    short = parts[n]
+
+    script = a[2]
+    group = short " " script
+
+    mem[group] += rss
+
+    if (!(group in example)) {
+      preview = $0
+      if (length(preview) > maxlen) {
+        preview = substr(preview, 1, maxlen) "..."
+      }
+      example[group] = preview
+    }
+  }
+  END {
+    for (i in mem) printf "%.2f GB - %s (%s)\n", mem[i]/1024/1024, i, example[i]
+  }' \
+  | sort -rn | head -n 20 \
+		| dmenu -p "pkill -15" -l 20 | awk -F ' - ' '{print $2}' \
 		| xargs pkill ;;
 	3) tmux-launch mem htop ;;
 esac
